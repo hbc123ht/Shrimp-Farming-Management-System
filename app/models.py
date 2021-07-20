e6168e3369d41a4d75efe77e36b3834b25078c7b
hbc123ht <hbc123ht@gmail.com>
2021-07-20 10:33:59 +0700

    add Parameters

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
 class Parameters(models.Model):
-    user = models.OneToOneField(User, verbose_name=_('user'), on_delete=models.CASCADE, primary_key=True)
-    ph = models.IntegerField(verbose_name=_('Ph_amount'))
-    temp = models.IntegerField(verbose_name=_('temperature'))
-    salinity = models.IntegerField(verbose_name=_('salinity'))
+    user = models.OneToOneField(User, verbose_name=_('user'), on_delete=models.CASCADE, primary_key=True, null=False, blank=False)
+    ph = models.IntegerField(verbose_name=_('Ph_amount'), null = True, blank = True)
+    temp = models.IntegerField(verbose_name=_('temperature'), null = True, blank = True)
+    salinity = models.IntegerField(verbose_name=_('salinity'), null = True, blank = True)
