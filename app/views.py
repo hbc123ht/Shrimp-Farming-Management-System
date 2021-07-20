414e99a2aee1ca3dc10489e0480d35a8d2769a7b
hbc123ht <hbc123ht@gmail.com>
2021-07-20 10:34:23 +0700

    update

--- app/views.py
+++ app/views.py
@@ -1,23 +1,41 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
+from django.views.generic.detail import DetailView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 import json
 
 from app.forms import RegisternForms
 from django.contrib.auth.models import User
+from app.models import Parameters
 
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, 'dashboard/index.html')
+    return render(request, 'dashboard/index.html', context = {})
+
+class ArticleDetailView(DetailView):
+
+    model = Parameters
+    template_name = 'dashboard/index.html'
+    context_object_name = 'params'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
 
 def RegisterView(request):
     if request.method == 'POST':
         form = RegisternForms(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            params = Parameters(user = user)
+            params.save()
+        
         return redirect('/')
     else:
         form = RegisternForms()
@@ -39,7 +57,11 @@ def update_params(request, username):
     if not user.check_password(request.data['password']):
         return Response(status.HTTP_401_UNAUTHORIZED)
 
+  
     
+
+    channel_layer = get_channel_layer()
+    async_to_sync(channel_layer.group_send)(username, { "type": "update.params", "text": {'Hiep' : 'CP'},})
     
     
 
---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+//    let socket = new WebSocket(`ws://127.0.0.1:8000/?session_key=${sessionKey}`);
+    var socket = new WebSocket(
+        'ws://' + window.location.host +
+        '/ws/' + CurrentUser + '/');
+  
+    socket.onmessage = function (e) {
+        
+    };
+});
