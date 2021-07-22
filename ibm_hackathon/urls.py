37d1978e87e4780ad0a31ff2c74f29bfa3bc9794
hbc123ht <hbc123ht@gmail.com>
2021-07-22 17:29:28 +0700

    for dashboard view

--- ibm_hackathon/urls.py
+++ ibm_hackathon/urls.py
@@ -20,7 +20,7 @@ from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'^$', views.index, name = 'home'),
+    re_path(r'^$', views.Dashboard.as_view(), name = 'home'),
     re_path(r'^login/$', auth_views.LoginView.as_view(), name='login'),
     re_path(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),
     re_path(r'^signup/$', views.RegisterView, name = 'signup'),
