88845978b1e01c3761a874bc9bf6f1faf01db2fb
hbc123ht <hbc123ht@gmail.com>
2021-07-24 16:31:47 +0700

    minor refactor

--- static/js/app.js
+++ static/js/app.js
@@ -110,7 +110,7 @@ function update_bar(quality){
 }
 
 $(document).ready(function () {
-//    let socket = new WebSocket(`ws://127.0.0.1:8000/?session_key=${sessionKey}`);
+    
     var socket = new WebSocket(
         'ws://' + window.location.host +
         '/ws/' + CurrentUser + '/');
